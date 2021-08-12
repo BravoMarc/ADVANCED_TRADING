ac1bfe5e9d9655619aa8264d2ee35d3664bda083
BravoMarc <m.bcharmes@gmail.com>
2021-08-12 18:20:16 +0200

    testing functions

--- test_0.0.0/fun_trade.py
+++ test_0.0.0/fun_trade.py
@@ -7,6 +7,7 @@ import json
 import pickle
 import traceback
 import math
+import pandas
 
 
 
@@ -19,3 +20,49 @@ def obtain_keys():
     f.close()
     
     return api_key , secret_key
+
+
+
+
+def RSI_fly(vector):
+    n=len(vector)
+    vec_down=[]
+    vec_up=[]
+    for i in range(1,n):
+        diff=vector[i]-vector[i-1]
+        if diff<0:
+            vec_down.append(abs(diff))
+            vec_up.append(0)
+        else:
+            vec_up.append(diff)
+            vec_down.append(abs(0))
+
+
+    RS=np.mean(vec_up)/np.mean(vec_down)
+    RSI=100-100/(1+RS)
+
+    return RSI
+
+    
+def rsi(df, periods = 14, ema = True):
+    """
+    Returns a pd.Series with the relative strength index.
+    """
+    close_delta = df['close'].diff()
+
+    # Make two series: one for lower closes and one for higher closes
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+    
+    if ema == True:
+	    # Use exponential moving average
+        ma_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+        ma_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+    else:
+        # Use simple moving average
+        ma_up = up.rolling(window = periods, adjust=False).mean()
+        ma_down = down.rolling(window = periods, adjust=False).mean()
+        
+    rsi = ma_up / ma_down
+    rsi = 100 - (100/(1 + rsi))
+    return rsi
--- test_0.0.0/test_data_generator.py
+++ test_0.0.0/test_data_generator.py
@@ -1,3 +1,6 @@
+
+from os import lchown
+import matplotlib
 import fun_trade as ft
 
 
@@ -9,9 +12,62 @@ import numpy as np
 import threading
 import traceback
 import pandas as pd
-
+from datetime import datetime as dt
+import time
+import matplotlib.pyplot as plt
+import talib as ta
+import pandas_ta as pdt
 
 api_key,secret_key = ft.obtain_keys()
 
 client = Client(api_key, secret_key)
 tickers=['LINKUSDT']
+key=tickers[0]
+
+actualtime=dt.today()
+interval='5m'
+t_end=int(actualtime.strftime('%s')) * 1000 
+t_start =int(actualtime.strftime('%s')) * 1000 - 5*14*5*60*1000
+bars = client.get_klines(symbol=key, interval=interval, startTime=t_start,endTime=t_end)
+historical=pd.DataFrame(columns=['CLOSE','High','low','date'], index=tickers)
+historical['CLOSE'][key]=[]
+historical['High'][key]=[]
+historical['low'][key]=[]
+historical['date'][key]=[]
+for line in bars:
+    historical['High'][key].append(float(line[3]))
+    historical['low'][key].append(float(line[3]))
+    historical['CLOSE'][key].append(float(line[4]))
+    historical['date'][key].append(time.strftime('%H:%M:%S', tm.localtime(line[6]/1000)))
+
+
+high=np.asarray(historical['High'][key])
+low =np.asarray(historical['low'][key])
+close=np.asarray(historical['CLOSE'][key])
+
+print(close[-1])
+print(ta.RSI(close,timeperiod=14)[-1] )
+print('')
+df=pd.Series(historical['CLOSE'][key])
+#print(pdt.rsi(df[0],length=14))
+
+slowk, slowd = ta.STOCH(high, low, close, fastk_period=14,
+ slowk_period=3, slowk_matype=0, slowd_period=3, slowd_matype=0)
+
+#print(slowk)
+print('')
+#print(slowd)
+dfhigh=pd.Series(historical['High'][key])
+dflow=pd.Series(historical['low'][key])
+
+stoch=pdt.stoch(dfhigh,dflow,df,k=14, d=3, smooth_k=3, mamode=None, offset=None,)
+print(stoch.iloc[-1,:])
+
+macd=pdt.macd(df)
+print(macd)
+
+macd, signal, hist=ta.MACD(close,fastperiod=12,slowperiod=26, signalperiod=9)
+
+print(macd[-1])
+print(signal[-1])
+print(hist[-1])
